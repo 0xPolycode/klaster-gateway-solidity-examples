2917d8a3a853394d28f7f886af71c751c732101b
fichiokaku <filip@polyflow.dev>
2023-10-19 11:25:10 +0200

    update

--- contracts/KlasterGatewayConsumer.sol
+++ contracts/KlasterGatewayConsumer.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.19;
+pragma solidity 0.8.20;
 
 import "https://github.com/0xPolycode/klaster-gateway-solidity-examples/blob/master/contracts/IKlasterGatewaySingleton.sol";
 
@@ -9,13 +9,15 @@ import "https://github.com/0xPolycode/klaster-gateway-solidity-examples/blob/mas
     Deploy this contract to any testnet chain and then start calling functions to execute
     cross-chain actions from your Klaster generated cross-chain wallet.
 
-    We recommend deploying to Mumbai as you can easily acquire test coins on their faucet
+    We recommend deploying this contract to Mumbai as you can easily acquire test coins on their faucet
     here: https://faucet.polygon.technology/
+
+    Then follow the steps below to play with Gateway.
 */
 contract KlasterGatewayConsumer {
    
     /**
-       STEP 1: Connect to the official Klaster Gateway testnet instance!
+       STEP 1: Connection to the official Klaster Gateway testnet instance obtained!
     */
     IKlasterGatewaySingleton public TESTNET_GATEWAY = IKlasterGatewaySingleton(
         0xdfF6fe22EACd7c3c4c9C3c9E9f9915026bBD98F1
@@ -23,22 +25,30 @@ contract KlasterGatewayConsumer {
 
     /**
        STEP 2: Generate you Klaster Gateway address by providing your wallet which is funded with
-               test coins, and a salt of your choice (for example, "my first wallet".
+               test coins, and a salt of your choice (for example, "my first wallet").
+               Call this function after deploying the contract to calculate the address.
     */
     function generateAddressExample(address caller, string memory salt) external view returns (address) {
         return TESTNET_GATEWAY.calculateAddress(caller, salt);
     }
 
     /**
-       STEP 3: Deploy a new ERC20 Token using the generated cross-chain wallet.
+       STEP 3: Call this function to deploy a new ERC20 Token using the generated cross-chain wallet.
                Provide the salt of the wallet you're deploying from (the same as in step1),
                and the target chain selector where the token will be deployed at.
                You can find chain selectors at the docs:
                
                https://klaster.gitbook.io/gateway/introduction/supported-chains#chain-selectors
+
+               IMPORTANT: you need to provide the `msg.value` when calling this function to pay for
+                          protocol fees. Calculate the protocol fee by calling the getDeployTokenExampleFee()
+                          and providing the same parameters as when actually calling this function.
     */
     function deployTokenExample() public {
 
+    }
+    function getDeployTokenExampleFee() external view returns (uint256) {
+        return 0;
     }
 
     /**
@@ -48,6 +58,15 @@ contract KlasterGatewayConsumer {
     */
     function batchDeployTokenCreate2Example() public {
 
+    }
+
+
+
+
+    // get the bytecode of the ERC20 token contract with input constructor values
+    function _getBytecode(address owner) private pure returns (bytes memory) {
+        bytes memory bytecode = type(KlasterGatewayWallet).creationCode;
+        return abi.encodePacked(bytecode, abi.encode(owner));
     }
 
 }
